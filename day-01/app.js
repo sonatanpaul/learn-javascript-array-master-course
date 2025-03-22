5289e29f4902bae92a6a853ba599174135f251cf
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-22 12:59:24 +0600

    how to declare array

---
+++ day-01/app.js
@@ -0,0 +1,19 @@
+console.log("Hello world");
+
+// How to create Array in Javascript
+/*
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers); // [1, 2, 3, 4, 5, 6];
+*/
+
+/*
+const friends = ["Anik", "Asraful", "Rayhan"];
+console.log(friends); // ['Anik', 'Asraful', 'Rayhan']
+*/
+
+// When we create Array with constractor and we pass one element, new Array array give me which number give me blank element
+/*
+const blankElement = new Array(10);
+console.log(blankElement); // (10) [empty × 10]
+*/
+
