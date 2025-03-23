3fc788a251d4b9792651cf338d05cce30f729127
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-23 18:01:16 +0600

    learn array filter method

--- day-01/app.js
+++ day-01/app.js
@@ -418,3 +418,67 @@ const arr3 = Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
   }
   */
 
+//   Array Method Start
+
+// The Customer Array
+let customers = [
+  {
+    id: 1,
+    f_name: "Abby",
+    l_name: "Thomas",
+    gender: "M",
+    married: true,
+    age: 32,
+    expense: 500,
+    purchased: ["Shampoo", "Toys", "Book"],
+  },
+  {
+    id: 2,
+    f_name: "Jerry",
+    l_name: "Tom",
+    gender: "M",
+    married: true,
+    age: 64,
+    expense: 100,
+    purchased: ["Stick", "Blade"],
+  },
+  {
+    id: 3,
+    f_name: "Dianna",
+    l_name: "Cherry",
+    gender: "F",
+    married: true,
+    age: 22,
+    expense: 1500,
+    purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+  },
+  {
+    id: 4,
+    f_name: "Dev",
+    l_name: "Currian",
+    gender: "M",
+    married: true,
+    age: 82,
+    expense: 90,
+    purchased: ["Book"],
+  },
+  {
+    id: 5,
+    f_name: "Maria",
+    l_name: "Gomes",
+    gender: "F",
+    married: false,
+    age: 7,
+    expense: 300,
+    purchased: ["Toys"],
+  },
+];
+
+// console.log(customers);
+
+// Array Filter() method :
+
+const seniorCitizen = customers.filter((customer) => customer.age >= 60);
+// console.log(seniorCitizen); // return (2) [{…}, {…}]
+const nonSeniorCitizen = customers.filter((customer) => customer.age < 60);
+// console.log(nonSeniorCitizen); // return (3) [{…}, {…}, {…}]
